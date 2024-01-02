7f872951b8338edf783a0e1445f071cb6ff32859
v0l0ru3v <f3ncykanal@gmail.com>
2024-01-02 17:06:17 +0300

    api added

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,61 @@
-import React from 'react';
-import { Space, Table, Tag } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
-
-interface DataType {
-  key: string;
-  company: string;
-  name: string;
-  age: number;
-  address: string;
-  tags: string[];
-  
-}
-
-const columns: ColumnsType<DataType> = [
-  {
-    title: 'Company',
-    dataIndex: 'company',
-    key: 'company',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
-
-const data: DataType[] = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-    company: 'Yandex',
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-    company: 'Yandex',
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-    company: 'Google',
-  },
-  {
-    key: '4',
-    name: 'Dmitry',
-    age: 20,
-    address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'developer'],
-    company: 'Google',
-  },
-];
-
-const App: React.FC = () => <Table columns={columns} dataSource={data} />;
-
-export default App;
+import React, { useState, useEffect } from 'react'; 
+import axios from 'axios'; 
+import { Table, Button } from 'antd'; 
+import { ColumnsType } from 'antd/lib/table'; 
+import DataType from './interfaces/data-type'; 
+ 
+const columns: ColumnsType<DataType> = [ 
+  { 
+    title: 'Страна', 
+    dataIndex: 'country', 
+    key: 'country', 
+  }, 
+  { 
+    title: 'Название школы', 
+    dataIndex: 'name', 
+    key: 'name', 
+  }, 
+]; 
+ 
+const PaginationComponent: React.FC = () => { 
+  const [data, setData] = useState<DataType[]>([]); 
+  const [page, setPage] = useState<number>(1); 
+  const limit: number = 10; 
+ 
+  useEffect(() => { 
+    fetchData(); 
+  }, [page]); 
+ 
+  const fetchData = async () => { 
+    const offset = (page - 1) * limit; 
+    try { 
+      const response = await axios.get(`http://universities.hipolabs.com/search?offset=${offset}&limit=${limit}`); 
+      setData(response.data); 
+    } catch (error) { 
+      console.error('Error fetching data: ', error); 
+    } 
+  }; 
+ 
+  const handlePrevPage = () => { 
+    if (page > 1) { 
+      setPage(page - 1); 
+    } 
+  }; 
+ 
+  const handleNextPage = () => { 
+    setPage(page + 1); 
+  }; 
+ 
+  const offset = (page - 1) * limit; 
+ 
+  return ( 
+    <> 
+      <Table dataSource={data} columns={columns} pagination={false} /> 
+      <Button onClick={handlePrevPage} disabled={!offset}>Назад</Button> 
+      <Button onClick={handleNextPage}>Вперед</Button> 
+      <span>Страница: {page}</span> 
+    </> 
+  ); 
+}; 
+ 
+export default PaginationComponent;
